10ca792d5f4bf0964d4553bf8ddaf27f97ee3dea
Mark Repka <mark@repkam09.com>
2022-08-21 20:12:33 -0400

    End of tutorial 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use bevy::{prelude::*, reflect::erased_serde::private::serde::__private::de};
+
+pub const DEFAULT_HEIGHT: f32 = 720.0;
+pub const DEFAULT_WIDTH: f32 = 1280.0;
+
 fn main() {
-    println!("Hello, world!");
+    // Uses the builder pattern. Defaults create a window, input handling, etc
+    // This is likely the only things you need in main for most projects
+    App::new()
+        .insert_resource(ClearColor(Color::BEIGE))
+        .insert_resource(WindowDescriptor {
+            width: DEFAULT_WIDTH,
+            height: DEFAULT_HEIGHT,
+            title: "Bevy Tower Defense".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+// Spawns a camera, should run once at startup via add_startup_system
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Spawn a plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+
+    // Sawn a cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+
+    // Spawn a light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
 }
